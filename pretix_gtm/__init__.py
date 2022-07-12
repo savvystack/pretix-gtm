5f0ecba9cb36da4b74a0d4f0763533a75c107130
Raymond Jia <raymond@savvystack.com>
2022-07-11 19:27:28 -0700

    Bootstrap with `cookiecutter https://github.com/pretix/pretix-plugin-cookiecutter`

---
+++ pretix_gtm/__init__.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext_lazy
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+__version__ = "1.0.0"
+
+
+class PluginApp(PluginConfig):
+    name = "pretix_gtm"
+    verbose_name = "Google Tag Manager"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Google Tag Manager")
+        author = "Raymond Jia"
+        description = gettext_lazy("Provides support for Google Tag Manager.")
+        visible = True
+        version = __version__
+        category = "INTEGRATION"
+        compatibility = "pretix>=4.10.0"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = "pretix_gtm.PluginApp"
